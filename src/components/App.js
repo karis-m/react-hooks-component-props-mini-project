216a4e14a8e521e5ee97c4238a428c50220e1045
moses <moses.kamau@student.moringaschool.com>
2023-07-20 14:52:56 +0300

    Complete Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,58 @@
 import React from "react";
 import blogData from "../data/blog";
 
-console.log(blogData);
+//console.log(blogData);
 
 function App() {
+  const Article = blogData.posts.map((postObj) => {
+    return <ArticleList key={postObj.id} title={postObj.title} date={postObj.date} preview={postObj.preview} />
+  })
   return (
     <div className="App">
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+    <Header/>
+    <About/>
+    <main>
+    {Article}
+    </main>
+    
     </div>
   );
 }
+function Header(){
+  return (
+    <div>
+      <header>
+        <h1>{blogData.name}</h1>
+      </header>
+    </div>
+  
+  )
+}
+function About({image = "https://via.placeholder.com/215"}){
+  return (
+    <div>
+      <aside>
+        <img src={image} alt="blog logo"/>
+        <p>{blogData.about}</p>
+      </aside>
+    </div>
+  
+  )
+}
+function ArticleList(props){
+return (
+  <div className="card">
+      <h3>{props.title}</h3>
+      <small>
+        {props.date}
+      </small>
+      <p>
+        {props.preview}
+      </p>
 
+    </div>
+)
+}
 export default App;
